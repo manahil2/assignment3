c2d19273a54207df6e63de0b7ed40c773dffb4bc
Manahil Aleem <manahilaleem1@gmail.com>
2024-12-15 17:38:36 +0500

    deploy

---
+++ script.js
@@ -0,0 +1,18 @@
+//1
+let age = 20;
+alert("I am " + age + " years old");
+
+//2
+let N = 8;
+alert("You have visited this site " + N + " times");
+
+//3
+let birthyear = 2004;
+document.write("My birth year is ", birthyear , "<br>");
+document.write("Data type of my declared variable is ",typeof(birthyear),"<br>");
+
+//4
+let visitorname = "John Doe";
+let producttitle = "T-shirts";
+let quantity = "5";
+document.write(visitorname+" ordered "+quantity+ " "+producttitle+" on XYZ Clothing store");
